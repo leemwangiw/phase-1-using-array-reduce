2b5e36e168d3f5c31e16c71c0391636a769be835
lee mwangi <lee.mwangi1@student.moringaschool.com>
2024-07-04 17:08:07 +0300

    commiting things

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Array of battery counts
+
+
+// Reducer function to sum up all batteries
+const totalBatteries = batteryBatches.reduce((acc, batch) => acc + batch, 0);
+
+console.log(totalBatteries); // 31
